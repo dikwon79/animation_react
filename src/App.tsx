b6f6a21e5a66775b640384cda50024d178ec4cec
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-12-03 13:55:42 +0900

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useEffect } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -18,15 +25,26 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { scale: 2, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-};
-
 function App() {
+  const x = useMotionValue(0);
+
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg , rgb(0,210,238), rgb(0,83,238))",
+      "linear-gradient(135deg , rgb(0,238,155), rgb(238,178,0))",
+    ]
+  );
+
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+    <Wrapper style={{ background: gradient }}>
+      <button onClick={() => x.set(200)}>Click me</button>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
